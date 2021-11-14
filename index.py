bb3ac717963ec5a7c966955cbdfd4d9b024a972e
Romeo <kilazlove@gmail.com>
2021-11-15 00:26:45 +0800

    git init

---
+++ index.py
@@ -0,0 +1,9 @@
+# Assignment 5 Program 3: Life stages
+#
+# Create a program that ask for an age of a person
+# Display the life stage of the person.
+# Rules:
+# 0 - 12 : Kid
+# 13 - 17 : Teen
+# 18 : Debut
+# 19 above : Adult
